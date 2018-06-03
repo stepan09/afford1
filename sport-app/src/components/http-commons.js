21f610e9c25ab7403497eed92ebc51d8d57b063b
stepan <stepanoliinyk09@gmail.com>
2018-06-04 00:27:20 +0300

    added http-commons

--- sport-app/src/components/http-commons.js
+++ sport-app/src/components/http-commons.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-export const AXIOS = axios.create({
+/*export const AXIOS = axios.create({
   baseURL: 'http://localhost:8080',
   headers: {
     'Access-Control-Allow-Origin': 'http://localhost:4200'
   }
-})
-/*export const AXIOS = axios.create({
-  baseURL: '/api'
 })*/
+export const AXIOS = axios.create({
+  baseURL: '/api'
+})
